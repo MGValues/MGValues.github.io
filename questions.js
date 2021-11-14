bfb31371690dfbd1f7f04bdecc394d3d8a03536a
MGValues <85768166+MGValues@users.noreply.github.com>
2021-11-13 18:59:04 -0600

    Update questions.js

--- questions.js
+++ questions.js
@@ -13,9 +13,17 @@ questions =  [
     
     {"id": 9, "question": "Court discussion channels should be private.", "effects": {"mod": 1}},
     {"id": 10, "question": "More pre-reset parts of the server should be added.", "effects": {"fed": 1}},
-    {"id": 11, "question": "Economic activity will be better sparked by promoting individual activity than by instating group policies such as taxation.", "effects": {"fed": 1}},
+    {"id": 11, "question": "Economic activity will be better sparked by promoting individual activity than by instating group policies such as taxation.", "effects": {"ali": 1}},
     
     {"id": 12, "question": "The NGEC channel should be viewable by the public to ensure transparency.", "effects": {"mod": -1}},
     {"id": 13, "question": "The idea of sub-regional positions is flawed.", "effects": {"fed": -1}},
     {"id": 14, "question": "We need to provide and utilize monetary institutions.", "effects": {"ali": -1}},
+    
+    {"id": 15, "question": "The courts should be given less power.", "effects": {"mod": 1}},
+    {"id": 16, "question": "Regions were implemented well post-reset.", "effects": {"fed": 1}},
+    {"id": 17, "question": "A regular loss of money via taxation in the economy will reduce incentive to participate in the economy.", "effects": {"ali": 1}},
+    
+    {"id": 18, "question": "Jolly was the sole cause of instability on September 29th, 2021.", "effects": {"mod": -1}},
+    {"id": 19, "question": "Regions currently hold a sufficient amount of power.", "effects": {"fed": -1}},
+    {"id": 20, "question": "Reforming the shop is not an effective way to create economic activity.", "effects": {"ali": -1}},
 ];
