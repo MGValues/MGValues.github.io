d99d587f966b79b7b62d850d39e713df46de98a7
MGValues <85768166+MGValues@users.noreply.github.com>
2021-12-28 12:07:45 -0600

    Update questions.js

--- questions.js
+++ questions.js
@@ -2,35 +2,27 @@ questions =  [
     {"id": 0, "question": "Those who recieve punishments should not have what they were punished for held against them after they've served their punishment.", "effects": {"mod": -1}},
     {"id": 1, "question": "The idea of Regional Courts is flawed.", "effects": {"fed": -1}},
     {"id": 2, "question": "We should not be afraid to implement taxes if necessary.", "effects": {"ali": -1}},
-    
     {"id": 3, "question": "The House of Representatives should be the only legislative body that may vote on bills.", "effects": {"mod": 1}},
     {"id": 4, "question": "The general community should be prioritized over individual rights.", "effects": {"fed": 1}},
     {"id": 5, "question": "Integrating the economy further into the Mock Government will create economic activity.", "effects": {"ali": 1}},
-    
     {"id": 6, "question": "Reform should be the top priority of any Presidential administration.", "effects": {"mod": -1}},
     {"id": 7, "question": "The electoral college election system has failed without any way of redemption.", "effects": {"fed": -1}},
     {"id": 8, "question": "Mock Government businesses should pay a portion of their profit in taxes to the treasury to create a stronger national bank.", "effects": {"ali": -1}},
-    
     {"id": 9, "question": "Court discussion channels should be private.", "effects": {"mod": 1}},
     {"id": 10, "question": "We should revisit and readd old parts of the Mock Government.", "effects": {"fed": 1}},
     {"id": 11, "question": "Economic activity will be better sparked by promoting individual activity than by instating group policies such as taxation.", "effects": {"ali": 1}},
-    
     {"id": 12, "question": "The NGEC channel should be viewable by the public to ensure transparency.", "effects": {"mod": -1}},
     {"id": 13, "question": "The idea of sub-regional positions is flawed.", "effects": {"fed": -1}},
     {"id": 14, "question": "We need to provide and utilize monetary institutions.", "effects": {"ali": -1}},
-    
     {"id": 15, "question": "The courts should be given less power.", "effects": {"mod": 1}},
     {"id": 16, "question": "Regions were implemented well post-reset.", "effects": {"fed": 1}},
     {"id": 17, "question": "A regular loss of money via taxation in the economy will reduce incentive to participate in the economy.", "effects": {"ali": 1}},
-    
     {"id": 18, "question": "Jolly was the sole cause of instability on September 29th, 2021.", "effects": {"mod": -1}},
     {"id": 19, "question": "Regions currently hold a sufficient amount of power.", "effects": {"fed": -1}},
     {"id": 20, "question": "Reforming the shop is not an effective way to create economic activity.", "effects": {"ali": -1}},
-    
     {"id": 21, "question": "Long term goals are more important than short term goals.", "effects": {"mod": 1}},
     {"id": 22, "question": "Elected Representatives and Governors should pay more attention to their regions than their work in the House of Representatives.", "effects": {"fed": 1}},
     {"id": 23, "question": "The current system of role income doesn't negatively affect the economy.", "effects": {"ali": 1}},
-    
     {"id": 24, "question": "Effective communication with staff will never benefit the Mock Government.", "effects": {"mod": -1}},
     {"id": 25, "question": "Giving regions too much power can be dangerous.", "effects": {"fed": -1}},
     {"id": 26, "question": "Regions should have their own economies.", "effects": {"ali": -1}},
